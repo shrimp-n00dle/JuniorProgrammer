9698cdbdbb347e07e5caa9654711d98aea1b4953
shrimp-n00dle <vingno.elizabeth@gmail.com>
2026-04-09 10:01:07 +0800

    Lesson 1.2 Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 20.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move tehe vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
